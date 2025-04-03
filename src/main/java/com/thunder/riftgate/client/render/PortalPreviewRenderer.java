eb4ee7a1a3955f206aba7e2877e4b55e73e14af3
Thunderrock424242 <masonjoshua8@gmail.com>
2025-04-03 12:27:42 -0400

    fixed some rendering code

--- src/main/java/com/thunder/riftgate/client/render/PortalPreviewRenderer.java
+++ src/main/java/com/thunder/riftgate/client/render/PortalPreviewRenderer.java
@@ -1,18 +1,21 @@
 package com.thunder.riftgate.client.render;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.ByteBufferBuilder;
 import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import com.thunder.riftgate.teleport.RoomManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.DoorBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 
 public class PortalPreviewRenderer implements BlockEntityRenderer<BlockEntity> {
@@ -20,11 +23,10 @@ public class PortalPreviewRenderer implements BlockEntityRenderer<BlockEntity> {
     public PortalPreviewRenderer(BlockEntityRendererProvider.Context context) {}
 
     @Override
-    public void render(BlockEntity blockEntity, float partialTicks, PoseStack poseStack,
-                       net.minecraft.client.renderer.MultiBufferSource buffer, int combinedLight, int combinedOverlay) {
+    public void render(BlockEntity blockEntity, float partialTicks, @NotNull PoseStack poseStack,
+                       net.minecraft.client.renderer.@NotNull MultiBufferSource buffer, int combinedLight, int combinedOverlay) {
 
         BlockState state = blockEntity.getBlockState();
-
         if (!(state.getBlock() instanceof DoorBlock)) return;
 
         boolean isOpen = state.getValue(DoorBlock.OPEN);
@@ -36,24 +38,27 @@ public class PortalPreviewRenderer implements BlockEntityRenderer<BlockEntity> {
         var playerId = mc.player.getUUID();
         PortalRenderManager.renderPortalPreview(playerId, blockEntity.getBlockPos());
         var fb = PortalRenderManager.getOrCreateFramebuffer(playerId);
+        int textureId = fb.getColorTextureId();
 
         poseStack.pushPose();
-        poseStack.translate(0.25, 0.5, 0.501); // door frame inset
+        poseStack.translate(0.25, 0.5, 0.501); // Inside door frame
         poseStack.scale(0.5F, 1.0F, 1.0F);
-
-        RenderSystem.setShaderTexture(0, fb.getColorTextureId());
         Matrix4f matrix = poseStack.last().pose();
 
-        Tesselator tess = Tesselator.getInstance();
-        BufferBuilder bb = tess.getBuilder();
-        bb.begin(VertexFormat.Mode.QUADS, RenderType.TEXTURE.get().format());
+        RenderSystem.enableBlend();
+        RenderSystem.setShaderTexture(0, textureId);
 
-        bb.vertex(matrix, 0, 1, 0).uv(0, 1).endVertex();
-        bb.vertex(matrix, 1, 1, 0).uv(1, 1).endVertex();
-        bb.vertex(matrix, 1, 0, 0).uv(1, 0).endVertex();
-        bb.vertex(matrix, 0, 0, 0).uv(0, 0).endVertex();
+        ByteBufferBuilder byteBuffer = new ByteBufferBuilder(256);
+        BufferBuilder bb = new BufferBuilder(byteBuffer, VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
 
-        tess.end();
+        bb.addVertex(matrix, 0, 1, 0).setUv(0, 1);
+        bb.addVertex(matrix, 1, 1, 0).setUv(1, 1);
+        bb.addVertex(matrix, 1, 0, 0).setUv(1, 0);
+        bb.addVertex(matrix, 0, 0, 0).setUv(0, 0);
+
+        var fallback = ResourceLocation.withDefaultNamespace("textures/misc/white.png");
+        mc.renderBuffers().bufferSource().endBatch(RenderType.text(fallback));
+
         poseStack.popPose();
     }
 }
